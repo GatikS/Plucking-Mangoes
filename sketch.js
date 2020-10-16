9889243503a876e63954fe4bcd55ff5f7f141cac
Gatik Shelar <70143599+GatikS@users.noreply.github.com>
2020-10-16 23:01:42 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,37 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var stone
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+    stone=new Stone()
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  
+  drawSprites();
+ 
+}
+
+
+
